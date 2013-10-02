ea413206b177341677aed92927061862950a6608
David Fedele <dave.fedele@gmail.com>
2013-10-01 19:58:04 -0700

    updating blackjack

--- week01/blackjack.rb
+++ week01/blackjack.rb
@@ -2,6 +2,14 @@ def message msg
 	puts "#{msg}"
 end
 
+def stay_or_hit(name)
+	puts ""
+	puts "#{name}, what would you like to do?"
+	puts "1) Hit"
+	puts "2) Stay"
+	puts ""
+end
+
 def calculate_total(hand)
 	arr = hand.map {|e| e[1] }
 	total = 0
@@ -34,8 +42,8 @@ message ''
 message 'Please practice responsible gambling...always go all in! :)'
 
 message 'Please enter your name: '
-player = gets.chomp
-message "Welcome #{player.downcase.capitalize}!"
+player = gets.chomp.downcase.capitalize
+message "Welcome #{player}!"
 
 #deal cards
 
@@ -53,17 +61,76 @@ my_total = calculate_total(player_hand)
 #show cards
 
 message "The dealer has: #{dealer_hand[0]} and #{dealer_hand[1]} for a total of #{dealer_total}"
-message "You have: #{player_hand[0]} and #{player_hand[1]} for a total of: #{my_total}"
+message "#{player}, you have: #{player_hand[0]} and #{player_hand[1]} for a total of: #{my_total}"
+message ""
 
-puts ""
-puts "What would you like to do?"
-puts "1) Hit"
-puts "2) Stay"
+if my_total == '21'
+	message "Blackjack!! You win #{player}!"
+	exit
+end
 
-action = gets.chomp
+while my_total < 22
+	stay_or_hit(player)
+	action = gets.chomp
 
-#if action == '1'
-#	player_hand << deck.pop
-#end
+	if action == '2'
+		#message "You have a total of: #{my_total}"
+		break
+	end	
 
+	player_hand << deck.pop
+	my_total = calculate_total(player_hand)
+	message "#{player}, you have a total of: #{my_total}"
 
+	if my_total == '21'
+		message "Blackjack!! You win #{player}!"
+		exit
+	elsif my_total > 22
+		message "Bust! You are over 21. Better luck next time #{player}!"
+		exit
+	end
+end
+
+#repeat code above for dealer
+if dealer_total == '21'
+	message "Dealer has Blackjack!! You lose! #{player} :("
+	exit
+end
+
+while dealer_total < 17
+	message "The Dealer hits."
+	dealer_hand << deck.pop
+	dealer_total = calculate_total(dealer_hand)
+	message "Dealer has a total of: #{dealer_total}"
+
+	if dealer_total == '21'
+		message "Dealer has Blackjack!! You lose #{player}!"
+		exit
+	elsif dealer_total > '21'
+		message "Bust! Dealer is over 21. You win #{player}!"
+		exit
+	end
+end
+
+#determine the winner
+message "The Dealer has #{dealer_total}"
+message "The Dealer's cardsa are: "
+dealer_hand.each do |card|
+	puts "=> #{card}"
+end
+
+message "#{player}, your total is #{my_total}"
+message "#{player}, your cards are: "
+player_hand.each do |card|
+	puts "=> #{card}"
+end
+
+message ""
+
+if dealer_total > my_total
+	message "The Dealer won! :("
+elsif dealer_total < my_total
+	message "You won #{player}!! Good job!"
+else
+	message "It's a push!"
+end
