cf2995aa65bbf8f853b1bc884db4745347d0e767
David Fedele <dave.fedele@gmail.com>
2013-10-01 22:42:25 -0700

    fixed some errors with comparisons

--- week01/blackjack.rb
+++ week01/blackjack.rb
@@ -34,65 +34,67 @@ end
 suit = ['H', 'D' , 'S', 'C']
 card = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-deck = suit.product(card)
+#create 2 decks and shuffle
+deck = suit.product(card) * 2
 deck.shuffle!
 
 message 'Welcome to Blackjack!'
 message ''
 message 'Please practice responsible gambling...always go all in! :)'
 
+message ''
 message 'Please enter your name: '
 player = gets.chomp.downcase.capitalize
 message "Welcome #{player}!"
 
-#deal cards
-
 player_hand=[]
 dealer_hand=[]
 
+#deal cards
 2.times do
 	player_hand << deck.pop
 	dealer_hand << deck.pop
 end
 
+#calculate total
 dealer_total = calculate_total(dealer_hand)
-my_total = calculate_total(player_hand)
+player_total = calculate_total(player_hand)
 
 #show cards
-
+message ''
 message "The dealer has: #{dealer_hand[0]} and #{dealer_hand[1]} for a total of #{dealer_total}"
-message "#{player}, you have: #{player_hand[0]} and #{player_hand[1]} for a total of: #{my_total}"
-message ""
+message "#{player}, you have: #{player_hand[0]} and #{player_hand[1]} for a total of: #{player_total}"
+message ''
 
-if my_total == '21'
+if player_total == 21
 	message "Blackjack!! You win #{player}!"
 	exit
 end
 
-while my_total < 22
+while player_total < 22
 	stay_or_hit(player)
 	action = gets.chomp
 
 	if action == '2'
-		#message "You have a total of: #{my_total}"
+		#message "You have a total of: #{player_total}"
 		break
 	end	
 
 	player_hand << deck.pop
-	my_total = calculate_total(player_hand)
-	message "#{player}, you have a total of: #{my_total}"
+	player_total = calculate_total(player_hand)
+	message "#{player}, you have a total of: #{player_total}"
 
-	if my_total == '21'
+	if player_total == 21
 		message "Blackjack!! You win #{player}!"
 		exit
-	elsif my_total > 22
+	elsif player_total > 22
 		message "Bust! You are over 21. Better luck next time #{player}!"
 		exit
 	end
 end
 
 #repeat code above for dealer
-if dealer_total == '21'
+if dealer_total == 21
 	message "Dealer has Blackjack!! You lose! #{player} :("
 	exit
 end
@@ -103,23 +105,25 @@ while dealer_total < 17
 	dealer_total = calculate_total(dealer_hand)
 	message "Dealer has a total of: #{dealer_total}"
 
-	if dealer_total == '21'
+	if dealer_total == 21
 		message "Dealer has Blackjack!! You lose #{player}!"
-		exit
-	elsif dealer_total > '21'
+		break
+	elsif dealer_total > 21
 		message "Bust! Dealer is over 21. You win #{player}!"
-		exit
+		break
 	end
 end
 
 #determine the winner
+message ''
 message "The Dealer has #{dealer_total}"
-message "The Dealer's cardsa are: "
+message "The Dealer's cards are: "
 dealer_hand.each do |card|
 	puts "=> #{card}"
 end
 
-message "#{player}, your total is #{my_total}"
+message ''
+message "#{player}, your total is #{player_total}"
 message "#{player}, your cards are: "
 player_hand.each do |card|
 	puts "=> #{card}"
@@ -127,9 +131,9 @@ end
 
 message ""
 
-if dealer_total > my_total
+if dealer_total > player_total
 	message "The Dealer won! :("
-elsif dealer_total < my_total
+elsif dealer_total < player_total
 	message "You won #{player}!! Good job!"
 else
 	message "It's a push!"
